952fc09a59bfe56b9f2bbf4a2b7aaf7f2a3d7b88
SebastianSlowakiewicz <14609@eppuz.edu.pl>
2024-04-03 12:03:33 +0200

    Convert menu to dynamic + Include books contents

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<?php
-echo('
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -10,45 +8,42 @@ echo('
     <title>Pan Tadeusz</title>
 </head>
 <body>
-<header class="container">
-<h1 class="text-center container">Pan Tadeusz, czyli ostatni zajazd na Litwie: historia szlachecka z roku 1811 i 1812 we dwunastu księgach wierszem</p></h1>
-<h2 class="text-end">Adam Mickiewicz</h2>
-</header>
-</body>
-  <div class="container text">
+  <header class="container">
+    <h1 class="text-center">Pan Tadeusz, czyli ostatni zajazd na Litwie: historia szlachecka z roku 1811 i 1812 we dwunastu księgach wierszem</h1>
+    <h2 class="text-end">Adam Mickiewicz</h2>
+  </header>
+  <div class="container">
     <div class="row">
       <div class="col-3">
         <div class="list-group">
-        <a class="list-group-item list-group-item-primary"><i class="bi bi-list-columns"></i>Spis treści</a>
-        <a href="./" class="list-group-item list-group-item-action"><i class="bi bi-house-fill"></i>Strona główna</a>
-    <a href="./k1.html" class="list-group-item list-group-item-action"><i class="bi bi-bookmark-fill"></i>Księga 1</a>
-    <a href="./k2.html" class="list-group-item list-group-item-action"><i class="bi bi-bookmark-fill"></i>Księga 2</a>
-    <a href="./k3.html" class="list-group-item list-group-item-action"><i class="bi bi-bookmark-fill"></i>Księga 3</a>
-    <a href="./k4.html" class="list-group-item list-group-item-action"><i class="bi bi-bookmark-fill"></i>Księga 4</a>
-    <a href="./k5.html" class="list-group-item list-group-item-action"><i class="bi bi-bookmark-fill"></i>Księga 5</a>
-    <a href="./k6.html" class="list-group-item list-group-item-action"><i class="bi bi-bookmark-fill"></i>Księga 6</a>
-    <a href="./k7.html" class="list-group-item list-group-item-action"><i class="bi bi-bookmark-fill"></i>Księga 7</a>
-    <a href="./k8.html" class="list-group-item list-group-item-action"><i class="bi bi-bookmark-fill"></i>Księga 8</a>
-    <a href="./k9.html" class="list-group-item list-group-item-action"><i class="bi bi-bookmark-fill"></i>Księga 9</a>
-    <a href="./k10.html" class="list-group-item list-group-item-action"><i class="bi bi-bookmark-fill"></i>Księga 10</a>
-    <a href="./k11.html" class="list-group-item list-group-item-action"><i class="bi bi-bookmark-fill"></i>Księga 11</a>
-    <a href="./k12.html" class="list-group-item list-group-item-action"><i class="bi bi-bookmark-fill"></i>Księga 12</a>
-    <div class="col=9">
-      <img src="./pantadeusz.jpeg" class="img-fluid rounded" alt="...">
-</div>
-<div class="col-6"><div><img src="./pantadeusz.jpeg" class="rounded mx-auto d-block" alt="..."></div>
-</div>
-</div>
-<div class="card">
-    <div class="card-footer"> Sebastian Słowakiewicz, Akademia nauk stosowanych w Nowym Targu
-    </div>
-    <div class="card-body">
-      <h5 class="card-title"></h5>
-      <p class="card-text">Pan Tadeusz</p>
-      <a href="#" class="btn btn-primary"></a>
-    </div>
-  </div>
-</body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</html>
-')
+          <a class="list-group-item list-group-item-success"><i class="bi bi-list-columns"></i>Spis treści</a>
+          <a href="./" class="list-group-item list-group-item-action"><i class="bi bi-house-fill"></i>Strona główna</a>
+          <?php
+          for ($i = 1; $i <= 12; $i++) {
+            // echo ('<a href="./k'.$i.'.html" class="list-group-item list-group-item-action"><i class="bi bi-bookmark-fill"></i>Księga '.$i.'</a>');
+            echo("<a href='./?k=$i' class='list-group-item list-group-item-action'><i class='bi bi-bookmark-fill'></i>Księga $i</a>");
+          }
+          ?>
+          </div>
+        </div>
+        <div class="col-9">
+          <?php
+          if(isset($_GET['k'])) {
+            include_once("k".$_GET['k'].".html");
+          } 
+          elseif(!empty($argv[1])) {
+            include_once("k".$_GET['k'].".html");
+          } 
+          else {
+            echo"<img src='./pantadeusz.jpeg' alt='Pan Tadeusz' class='img-fluid rounded'>";
+          }
+          ?>
+          </div>
+        </div>
+        <footer class="container fixed-bottom alert alert-success text-end">
+          (c) Sebastian Słowakiewicz | Akademia nauk stosowanych w Nowym Targu
+        </footer>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+      </body>
+      </html>
+
